e2908fb2943789fb92fd59fb7faa6a9d6ab3efc9
clarkdave <git@clarkdave.net>
2023-04-14 00:45:50 +0100

    fix build script

--- bin/build-firefox.sh
+++ bin/build-firefox.sh
@@ -6,13 +6,13 @@ source .env
 pnpm run clean
 pnpm parcel build manifest.json --target webext-prod
 
-web-ext lint \
+npx web-ext lint \
   --source-dir ./dist/webext-prod
 
-web-ext build \
+npx web-ext build \
   --source-dir ./dist/webext-prod
 
-web-ext sign \
+npx web-ext sign \
   --source-dir ./dist/webext-prod \
   --api-key="$WEB_EXT_API_KEY" \
   --api-secret="$WEB_EXT_API_SECRET"
